7457b651039369999af4dc1a051ee5319de6f48f
stpCollabr8nLstn <amrios2817@gmail.com>
2019-05-03 02:36:37 -0400

    feat(index): add component and query to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
-import React from "react"
+import React from 'react';
+import { graphql } from 'gatsby';
+import Seat from '../components/Seat';
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => {
+  const seats = data.allSeat.edges;
+  return (
+    <div>
+      {seats.map(seat => (
+        <Seat />
+      ))}
+    </div>
+  )
+}
+
+export const query = graphql`
+  query AllSeatsQuery {
+    allSeat {
+      edges {
+        node {
+          row
+          seat
+          occupied
+          class
+        }
+      }
+    }
+  }
+`;
